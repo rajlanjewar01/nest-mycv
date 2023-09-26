d5c47d27d5139dc08837015da57843efc855c206
rajperficient01 <rajl.perficient@gmail.com>
2023-09-26 13:49:37 +0530

    adding delete and update logic

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post, Get, Patch, Param, Query  } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete  } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -11,7 +12,7 @@ export class UsersController {
         this.userService.create(body.email, body.password);
     }
 
-    //find user by perticular id
+    //find user by particular id
     @Get('/:id')
     findUser(@Param('id') id: string) {
         return this.userService.findOne(parseInt(id)); //converting string id to number
@@ -22,4 +23,17 @@ export class UsersController {
     findAllUser(@Query('email') email:string) {
         return this.userService.find(email);
     }
+
+    //delete user with specific id
+    @Delete('/:id')
+    deleteUser(@Param('id') id: string) {
+        return this.userService.remove(parseInt(id));
+    }
+
+    //update user
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) { 
+        return this.userService.update(parseInt(id), body);
+    }
+
 }
